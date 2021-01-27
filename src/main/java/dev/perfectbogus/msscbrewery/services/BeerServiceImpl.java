8ad999f99249fb674b1c61969016fbbd001e4c36
eduardo escobar <war_army_10@hotmail.com>
2021-01-27 12:23:32 -0600

    HTTP Post with Spring MVC

--- src/main/java/dev/perfectbogus/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/dev/perfectbogus/msscbrewery/services/BeerServiceImpl.java
@@ -15,4 +15,9 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder().id(UUID.randomUUID()).build();
+    }
 }
