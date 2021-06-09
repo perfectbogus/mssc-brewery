0aed8b0d978f6b5c3be45a36dd5068bbb3777365
eduardo escobar <war_army_10@hotmail.com>
2021-06-09 12:14:47 -0500

    Step 78: Example how to use mapstruct with lombok

---
+++ src/main/java/dev/perfectbogus/msscbrewery/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package dev.perfectbogus.msscbrewery.web.mappers;
+
+import dev.perfectbogus.msscbrewery.domain.Beer;
+import dev.perfectbogus.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface  BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
