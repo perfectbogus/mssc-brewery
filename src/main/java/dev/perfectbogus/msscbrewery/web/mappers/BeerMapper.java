a6fc37dda12e294953f5b5875b6cafb999e6af63
eduardo escobar <war_army_10@hotmail.com>
2021-06-09 19:34:14 -0500

    Step 81: Creating example for use mapstruct

--- src/main/java/dev/perfectbogus/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/dev/perfectbogus/msscbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import dev.perfectbogus.msscbrewery.domain.Beer;
 import dev.perfectbogus.msscbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface  BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
